79394d5cf09fffd0a2d2fa3fa1f13a6754b6c326
OlegVz <vozniukos@gmail.com>
2021-03-17 23:00:05 +0200

    Introducing Threading

--- src/main/java/com/hybris/internship/Application.java
+++ src/main/java/com/hybris/internship/Application.java
@@ -1,4 +1,24 @@
 package com.hybris.internship;
 
 public class Application {
+
+    public static void main(String[] args) {
+
+        Task taskRunner = new Task();
+        taskRunner.start();
+
+        System.out.println("Hello there..");
+
+        Task taskRunner2 = new Task();
+        taskRunner2.start();
+    }
+}
+
+class Task extends Thread {
+
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("number: " + i);
+        }
+    }
 }
