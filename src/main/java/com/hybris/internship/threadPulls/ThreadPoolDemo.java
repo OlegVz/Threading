d88bfe9ddbe9a677e33cd21c1cf254266c26eb20
OlegVz <vozniukos@gmail.com>
2021-03-18 19:59:10 +0200

    Thread Pools and the Executor Framework

---
+++ src/main/java/com/hybris/internship/threadPulls/ThreadPoolDemo.java
@@ -0,0 +1,36 @@
+package com.hybris.internship.threadPulls;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolDemo {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        Runnable processor1 = new MessageProcessor(2);
+        executor.execute(processor1);
+
+        Runnable processor2 = new MessageProcessor(3);
+        executor.execute(processor2);
+
+        Runnable processor3 = new MessageProcessor(4);
+        executor.execute(processor3);
+
+        Runnable processor4 = new MessageProcessor(5);
+        executor.execute(processor4);
+
+        executor.shutdown();
+//        executor.shutdownNow();
+
+//        while (!executor.isTerminated()) {}
+        try {
+            executor.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Submitted all tasks...");
+    }
+}
